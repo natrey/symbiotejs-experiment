efbf89767206494d80e78361fc29f9be66fbc40e
n.andreychenko <natandreychenko@gmail.com>
2022-04-12 14:15:26 +0300

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    requireConfigFile: false,
+  },
+  env: {
+    browser: true,
+  },
+  ignorePatterns: ['dist/*'],
+  extends: '@funboxteam',
+};
--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, '..', 'dist'), 
-        },
-        hot: true,
-        compress: true,
-        port: 3000,
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '..', 'dist'),
     },
+    hot: true,
+    compress: true,
+    port: 3000,
+  },
 };
